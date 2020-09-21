57bbab9cf7270c334ce458b576b1e46753c7394a
Gabby Broussard <gabriella.b.broussard@gmail.com>
2020-09-21 15:40:41 -0500

    prepare.py

--- prepare.py
+++ prepare.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[64]:
 
 
 import pandas as pd
@@ -18,56 +18,102 @@ warnings.filterwarnings("ignore")
 from acquire import get_telco_data
 
 
-# In[3]:
+# In[93]:
 
 
 telco=get_telco_data()
+
+
+# In[91]:
+
+
+#Getting just a look at what the data frame looks like by using .head()
 telco.head()
 
 
-# In[17]:
+# In[94]:
 
 
+# Describing the numerical data
+telco.describe()
+
+
+# In[66]:
+
+
+# removing excess columns
 telco= telco.drop(columns='Unnamed: 0')
 telco= telco.drop(columns='internet_service_type_id.1')
 telco= telco.drop(columns='payment_type_id')
 telco= telco.drop(columns='contract_type_id.1')
 telco= telco.drop(columns='payment_type_id.1')
 telco= telco.drop(columns= 'contract_type_id')
+telco= telco.drop(columns='internet_service_type_id')
 
 
-# In[18]:
+# In[67]:
 
 
+#making single variables
+telco['years_tenure'] = telco.tenure / 12
+telco['has_streaming']= telco["streaming_tv" or "streaming_movies"] == 'Yes'
+telco['is_family']=telco["partner" or "dependents"] == 'Yes'
+telco['has_phones']= telco['phone_service' or 'multiple_lines']== 'Yes'
+telco['has_security_features']= telco['online_security' or 'online_backup'] =='Yes'
+
+
+# In[68]:
+
+
+#double checking my work
 telco.head()
 
 
-# In[21]:
+# In[69]:
 
 
+#making dummy variables
 telco_dummies = pd.get_dummies(telco.churn, drop_first=True)
+
+
 telco_dummies.head(3)
 
 
-# In[24]:
+# In[70]:
 
 
+#concat the dummy variables to my df
 telco = pd.concat([telco, telco_dummies], axis=1)
 telco.head()
 
 
-# In[35]:
+# In[71]:
 
 
-def prep_telco(cached=True):
-    '''
-    This function acquires and prepares the iris data from a local csv, default.
-    Passing cached=False acquires fresh data from Codeup db and writes to csv.
-    Returns the iris df with dummy variables encoding species.
-    '''
+telco.info()
+
+
+# In[43]:
+
+
+telco.describe()
+
+
+# In[78]:
+
+
+##  This function acquires and prepares the telco data from a local csv, default.
+##  Passing cached=False acquires fresh data from sql and writes to csv.
+##  Returns the telco df with dummy variables encoding species.
+
+
+def clean_telco(cached=True):
+   
     
     # use my aquire function to read data into a df from a csv file
     df = get_telco_data()
+    # drop duplicates
+    df.drop_duplicates(inplace=True)
     
     # drop and rename columns
     df= df.drop(columns='Unnamed: 0')
@@ -76,6 +122,13 @@ def prep_telco(cached=True):
     df= df.drop(columns='contract_type_id.1')
     df= df.drop(columns='payment_type_id.1')
     df= df.drop(columns= 'contract_type_id')
+    df= df.drop(columns='internet_service_type_id')
+    df['years_tenure'] = df.tenure / 12
+    df['has_streaming']= df["streaming_tv" or "streaming_movies"] == 'Yes'
+    df['is_family']=df["partner" or "dependents"] == 'Yes'
+    df['has_phones']= df['phone_service' or 'multiple_lines']== 'Yes'
+    df['has_security_features']= df['online_security' or 'online_backup'] =='Yes'
+    df['years_tenure'] = df.tenure / 12
 
     
     # create dummy columns for churn
@@ -89,8 +142,44 @@ def prep_telco(cached=True):
     return df
 
 
-# In[36]:
+# In[82]:
 
 
-prep_telco()
+df=clean_telco()
+
+
+# In[87]:
+
+
+# Counting nulls in my clean data
+df.isnull().sum()
+
+
+# In[88]:
+
+
+#making my split, train, test data using is_churn
+train_validate, test = train_test_split(df, test_size=.2, 
+                                        random_state=123,
+                                        stratify=df.is_churn
+                                       )
+train, validate = train_test_split(train_validate, test_size=.3, 
+                                 random_state=123,
+                                 stratify=train_validate.is_churn
+                                        )
+
+
+# In[95]:
+
+
+#combining my split, train, test data and my clean data into one dataframe
+def prep_telco_data():
+    df = clean_data()
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123, stratify=df.is_churn)
+    train, validate = train_test_split(train_validate, 
+                                       test_size=.3, 
+                                       random_state=123, 
+                                       stratify=train_validate.is_churn)
+    train, validate, test = impute_mode()
+    return train, validate, test
 
